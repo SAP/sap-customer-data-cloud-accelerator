a4392ac5c3d1a9aebb179392a4849e890a2c0451
Ricardo Gaiao <ricardo.gaiao@sap.com>
2023-08-08 16:36:19 +0100

    feature/CDCINITIALIZER-43: "Fix deploy"

--- cdc-initializer/deploy/deployAcls.js
+++ cdc-initializer/deploy/deployAcls.js
@@ -1,24 +1,51 @@
-const fs = require('fs')
-const path = require('path')
-const { setAclRequest, getPartnerID } = require('../services/gigya/gigya.helpers')
-const {getPartnerId} = require('../utils/utils')
+/*
+ * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-initializer contributors
+ * License: Apache-2.0
+ */
 
-const deployAcls = async ({ gigya, apiKey, dataCenter, buildDirectory }) => {
-  const aclFiles = fs.readdirSync(buildDirectory)
+const fs = require('fs');
+const path = require('path');
+const { setAclRequest, getPartnerID } = require('../services/gigya/gigya.helpers');
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const deployAcls = async ({ gigya, apiKey, buildDirectory }) => {
+  const aclFiles = fs.readdirSync(buildDirectory);
+  const dataCenter = "us1";
+
+  const partnerIDResponse = await getPartnerID(gigya, {
+    query: `select partnerID, siteID, baseDomain from sites where apiKey="${apiKey}"`,
+    dataCenter,
+  });
+  
+  const partnerID = partnerIDResponse.data[0].partnerID;
+
+  if (!partnerID) {
+    console.error(`Failed to retrieve partnerID for apiKey "${apiKey}"`);
+    throw new Error("PartnerID is not available");
+  }
 
   for (const file of aclFiles) {
-    const filePath = path.join(buildDirectory, file)
-    const fileContents = fs.readFileSync(filePath, { encoding: 'utf8' })
+    const filePath = path.join(buildDirectory, file);
+    const fileContents = fs.readFileSync(filePath, { encoding: 'utf8' });
 
-    const aclValue = JSON.parse(fileContents)
-    const aclID = path.basename(file, '.json')
-   
-    const result = setAclRequest(gigya, { apiKey, dataCenter, aclID: aclID, acl: JSON.stringify(aclValue), partnerID: "79597568" })
-    
+    const aclValue = JSON.parse(fileContents);
+    const aclID = path.basename(file, '.json');
+
+    await delay(100); 
+
+    const result = setAclRequest(gigya, {
+      apiKey,
+      dataCenter,
+      aclID: aclID,
+      acl: JSON.stringify(aclValue),
+      partnerID: partnerID,
+    });
+
     if (result.errorCode) {
-      throw new Error(result.errorCode)
+      throw new Error(result.errorCode);
     }
   }
-}
+};
 
-module.exports = { deployAcls }
+module.exports = { deployAcls };
--- cdc-initializer/deploy/deployPermissionGroups.js
+++ cdc-initializer/deploy/deployPermissionGroups.js
@@ -2,29 +2,59 @@
  * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-initializer contributors
  * License: Apache-2.0
  */
+
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const { setPermissionGroupsRequest, getPartnerID } = require('../services/gigya/gigya.helpers');
 
-const { setPermissionRequest } = require('../services/gigya/gigya.helpers')
-const {getPartnerId} = require('../utils/utils')
-const deployPermissionGroups = async ({ gigya, apiKey, dataCenter, buildDirectory }) => {
-  const filePath = path.join(buildDirectory, 'permissionGroups.json')
+const deployPermissionGroups = async ({ gigya, apiKey, buildDirectory }) => {
+  const filePath = path.join(buildDirectory, 'permissionGroups.json');
 
-  if (fs.statSync(filePath).isFile()) {
-    const fileContents = fs.readFileSync(filePath, { encoding: 'utf8' })
+  const partnerIDResponse = await getPartnerID(gigya, {
+    query: `select partnerID, siteID, baseDomain from sites where apiKey="${apiKey}"`,
+    dataCenter:"us1",
+  });
+  const partnerID = partnerIDResponse.data[0].partnerID;
 
-    const permissionGroupValue = JSON.parse(fileContents)
-    const groupID = permissionGroupValue.aclID
-    const aclID = permissionGroupValue.aclID
-    const scope = JSON.stringify(permissionGroupValue.scope)
-
-
-
-    const result = setPermissionRequest(gigya, { apiKey, dataCenter, groupID: groupID, scope: scope, partnerID: "79597568", aclID: aclID })
-    if (result.errorCode) {
-      throw new Error(result.errorCode)
-    }
+  if (!partnerID) {
+    console.error(`Failed to retrieve partnerID for apiKey "${apiKey}"`);
+    throw new Error("PartnerID is not available");
   }
-}
 
-module.exports = { deployPermissionGroups }
+  if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+    const fileContents = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const permissionGroups = JSON.parse(fileContents);
+
+    for (const [groupID, permissionGroupValue] of Object.entries(permissionGroups)) {
+      if (!permissionGroupValue.aclID || !permissionGroupValue.scope) {
+        console.error('aclID or scope is missing in permissionGroups.json for group:', groupID);
+        throw new Error('aclID or scope is missing in permissionGroups.json');
+      }
+
+      try {
+        const result = await setPermissionGroupsRequest(gigya, {  
+          dataCenter: "us1", 
+          partnerID: partnerID, 
+          groupID: groupID, 
+          aclID: permissionGroupValue.aclID,
+          scope: JSON.stringify(permissionGroupValue.scope), 
+        });
+
+        if (result.errorCode && result.errorCode !== 400006) {
+          throw new Error(result.errorCode);
+        }
+      } catch (err) {
+        if (err.message === '400006') {
+          console.log(`Group ${groupID} already exists. Skipping...`);
+        } else {
+          console.log('Error creating group:', err.message);
+        }
+      }
+    }
+  } else {
+    console.error('PermissionGroups file does not exist');
+    throw new Error('PermissionGroups file does not exist');
+  }
+};
+
+module.exports = { deployPermissionGroups };
