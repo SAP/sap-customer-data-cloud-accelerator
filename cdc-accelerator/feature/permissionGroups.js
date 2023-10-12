bb17c2a4af77bb6fa2e883924f0201e348a3eead
Lopes <g.lopes@sap.com>
2023-10-12 14:13:29 +0100

    CDCINITIALIZER-41:Refactor permission groups

--- cdc-accelerator/feature/permissionGroups.js
+++ cdc-accelerator/feature/permissionGroups.js
@@ -4,7 +4,6 @@ import fs from 'fs'
 import { clearDirectoryContents } from '../utils/utils.js'
 import { SRC_DIRECTORY, BUILD_DIRECTORY } from './constants.js'
 import client from '../../cdc-accelerator/sap-cdc-toolkit/gigya/client.js'
-import 'dotenv/config'
 export default class PermissionGroups extends PartnerFeature {
     static PERMISSIONGROUP_FILE_NAME = 'permissionGroups.json'
     constructor(credentials) {
@@ -15,19 +14,17 @@ export default class PermissionGroups extends PartnerFeature {
         return this.constructor.name
     }
 
-    async init(apiKey, siteConfig, siteDirectory) {
-        const getPartnerID = siteConfig['partnerId']
-        const { USER_KEY, SECRET_KEY } = process.env
-        if (!getPartnerID || getPartnerID.errorCode) {
-            console.error(`Failed to retrieve partnerID for apiKey "${apiKey}"`)
-            throw new Error(JSON.stringify(getPartnerID))
+    async init(siteInfo, partnerDirectory) {
+        if (!siteInfo['partnerId']) {
+            console.error(`Failed to retrieve partnerID for apiKey "${siteInfo['apiKey']}"`)
+            throw new Error(JSON.stringify(siteInfo['partnerId']))
         }
 
-        const featureDirectory = path.join(siteDirectory, this.getName())
+        const featureDirectory = path.join(partnerDirectory, this.getName())
         this.createDirectory(featureDirectory)
 
         // Get permission groups
-        const permissionGroupsRes = await this.getPermissionGroups(apiKey, siteConfig['dataCenter'], getPartnerID, USER_KEY, SECRET_KEY)
+        const permissionGroupsRes = await this.getPermissionGroups(siteInfo['dataCenter'], siteInfo['partnerId'], this.credentials)
 
         if (permissionGroupsRes.errorCode) {
             throw new Error(JSON.stringify(permissionGroupsRes))
@@ -51,21 +48,14 @@ export default class PermissionGroups extends PartnerFeature {
     async deploy(directory) {
         console.log('deploy called')
     }
-    async getPermissionGroups(apiKey, dataCenter, partnerID, userKey, secret) {
+    async getPermissionGroups(dataCenter, partnerID, credentials) {
         const url = `https://admin.${dataCenter}.gigya.com/admin.getGroups`
-        const response = await client.post(url, this.#setPermissionGroupsParameters(apiKey, partnerID, userKey, secret)).catch((error) => error)
+        const response = await client.post(url, this.#setPermissionGroupsParameters(partnerID, credentials.userKey, credentials.secret)).catch((error) => error)
         return response.data
     }
-    // async set(apiKey, config, targetDataCenter) {
-    //     const url = UrlBuilder.buildUrl(Policy.#NAMESPACE, targetDataCenter, Policy.#SET_ENDPOINT)
-    //     const response = await client.post(url, this.#setPolicyConfigParameters(apiKey, config)).catch(function (error) {
-    //         return generateErrorResponse(error, Policy.#ERROR_SET_POLICY_CONFIG)
-    //     })
-    //     return response.data
-    // }
-    #setPermissionGroupsParameters(apiKey, partnerID, userKey, secret) {
+
+    #setPermissionGroupsParameters(partnerID, userKey, secret) {
         const parameters = Object.assign({})
-        parameters.apiKey = apiKey
         parameters.userKey = userKey
         parameters.secret = secret
         parameters.partnerID = partnerID
