6bd6857f8ac33ba59559ba766dfcf045e725eaca
Bruno Santos <b.santos@sap.com>
2023-10-25 18:39:41 +0100

    CDCINITIALIZER-129: Removed license comment from the template files

--- cdc-accelerator/templates/defaultWebSdk.js
+++ cdc-accelerator/templates/defaultWebSdk.js
@@ -1,7 +1,3 @@
-/*
- * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-initializer contributors
- * License: Apache-2.0
- */
 {
   // A comma-delimited list of provider names to enable.
   enabledProviders: '*',
