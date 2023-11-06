5741f94576b2419474b9c44c4206a0d5b05c656b
Lopes <g.lopes@sap.com>
2023-11-06 09:32:11 +0000

    CDCINITIALIZER-43:Permission Groups Deploy

--- cdc-accelerator/feature/permissionGroups.js
+++ cdc-accelerator/feature/permissionGroups.js
@@ -45,16 +45,63 @@ export default class PermissionGroups extends PartnerFeature {
         this.copyFileFromSrcToBuild(srcFeaturePath, PermissionGroups.PERMISSIONGROUP_FILE_NAME)
     }
 
-    async deploy(directory) {
-        console.log('deploy called')
+    async deploy(partnerDirectory, siteInfo) {
+        const buildFeatureDirectory = path.join(partnerDirectory, this.getName())
+        const buildFileName = path.join(buildFeatureDirectory, `${this.getName()}.json`)
+        const fileContent = fs.readFileSync(buildFileName, { encoding: 'utf8' })
+        const parsedContent = JSON.parse(fileContent)
+        const aclAndScopeData = []
+        if (!fileContent || !fileContent.length) {
+            throw new Error(`Invalid file: ${buildFileName}`)
+        }
+
+        for (const key in parsedContent) {
+            const keys = parsedContent[key]
+            let aclId = keys.aclID
+            let scope = keys.scope
+            aclAndScopeData.push({ aclId, scope })
+        }
+        aclAndScopeData.forEach(async ({ aclId, scope }) => {
+            const requestBody = {
+                aclId: aclId,
+                scope: scope,
+            }
+            const response = await this.deployPermissionGroup(siteInfo, requestBody.aclId, JSON.stringify(requestBody.scope), this.credentials)
+            if (response.errorCode === 400006) {
+                console.log(`Group ${requestBody.aclId} already exists. Skipping... `)
+            }
+            if (response.errorCode && response.errorCode !== 400006) {
+                throw new Error(JSON.stringify(response))
+            }
+        })
+    }
+
+    async deployPermissionGroup(siteInfo, aclID, scope, credentials) {
+        return await this.setPermissionRequest(siteInfo.dataCenter, siteInfo.partnerId, aclID, scope, credentials.userKey, credentials.secret)
     }
     async getPermissionGroups(dataCenter, partnerID, credentials) {
         const url = `https://admin.${dataCenter}.gigya.com/admin.getGroups`
-        const response = await client.post(url, this.#setPermissionGroupsParameters(partnerID, credentials.userKey, credentials.secret)).catch((error) => error)
+        const response = await client.post(url, this.#getPermissionGroupsParameters(partnerID, credentials.userKey, credentials.secret)).catch((error) => error)
+        return response.data
+    }
+    async setPermissionRequest(dataCenter, partnerID, aclID, scope, userKey, secret) {
+        const url = `https://admin.${dataCenter}.gigya.com/admin.createGroup`
+        const response = await client.post(url, this.#setPermissionGroupsParameters(partnerID, userKey, secret, aclID, scope)).catch((error) => error)
         return response.data
     }
 
-    #setPermissionGroupsParameters(partnerID, userKey, secret) {
+    #setPermissionGroupsParameters(partnerID, userKey, secret, aclID, scope) {
+        const parameters = Object.assign({})
+        parameters.userKey = userKey
+        parameters.secret = secret
+        parameters.partnerID = partnerID
+        parameters.groupID = aclID
+        parameters.aclID = aclID
+        parameters.scope = scope
+        return parameters
+    }
+
+    #getPermissionGroupsParameters(partnerID, userKey, secret) {
         const parameters = Object.assign({})
         parameters.userKey = userKey
         parameters.secret = secret
