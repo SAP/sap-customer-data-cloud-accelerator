f1b532e821cd4e03f8421cc8d70c8bcf533e5839
Luís Marques <l.marques@sap.com>
2023-11-13 17:45:00 +0000

    feature/CDCINITIALIZER-158: Changed src to src_ to then rename it back to src in order to change the directories first letter to uppercase

--- build/SAP Customer Data Cloud B2B/Sites/cdc-accelerator.parent-site.com/WebSdk/WebSdk.js
+++ build/SAP Customer Data Cloud B2B/Sites/cdc-accelerator.parent-site.com/WebSdk/WebSdk.js
@@ -1,28 +1,24 @@
 {
-    // A comma-delimited list of provider names to enable.
-    enabledProviders: '*',
+  // A comma-delimited list of provider names to enable.
+  enabledProviders: '*',
 
-    // Define the language of Gigya's user interface and error message.
-    lang: 'en',
+  // Define the language of Gigya's user interface and error message.
+  lang: 'en',
 
-    // Bind globally to events.
-    customEventMap: {
-        eventMap: [
-            {
-                events: '*',
-                args: [
-                    function (e) {
-                        return e;
-                    }
-                ],
-                method: function method(e) {
-                    if (e.fullEventName === 'login') {
-                        // Handle login event here.
-                    } else if (e.fullEventName === 'logout') {
-                        // Handle logout event here.
-                    }
-                }
-            }
-        ]
-    }
+  // Bind globally to events.
+  customEventMap: {
+    eventMap: [{
+      events: '*',
+      args: [function (e) {
+        return e;
+      }],
+      method: function method(e) {
+        if (e.fullEventName === 'login') {
+
+          // Handle login event here.
+        } else if (e.fullEventName === 'logout') {
+          // Handle logout event here.
+        }}
+    }]
+  }
 }
