e6f5ee99fc8fa3efb3a706f92c9adf3ed68d179a
Bruno Santos <b.santos@sap.com>
2023-11-21 14:57:00 +0000

    CDCINITIALIZER-37: Fix sonar issue, create Added cli module

--- cdc-accelerator/feature/accelerator.js
+++ cdc-accelerator/feature/accelerator.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-accelerator contributors
+ * License: Apache-2.0
+ */
+
 import readline from 'readline'
 import Feature from './feature.js'
 import { Operations, SRC_DIRECTORY } from './constants.js'
---
+++ cdc-accelerator/feature/commander.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-accelerator contributors
+ * License: Apache-2.0
+ */
+import { Operations } from './constants.js'
+import CLI from './cli.js'
+import { program } from 'commander'
+import { execSync } from 'child_process'
+
+export default class Commander {
+    static #BABEL_COMMAND = 'npx babel --delete-dir-on-start src -d build'
+    static #PRETTIER_COMMAND = 'npx prettier --semi true --trailing-comma none --write '
+    static #START_SERVER_COMMAND = 'npx light-server -c .lightserverrc'
+
+    async init(options) {
+        await new CLI().main(process, Operations.init, options.featureName, options.environment)
+    }
+
+    async #reset(options) {
+        await new CLI().main(process, Operations.reset, options.featureName, options.environment)
+    }
+
+    async #build(options) {
+        const command = `${Commander.#BABEL_COMMAND} && ${Commander.#PRETTIER_COMMAND} build/**/*.js`
+        execSync(command, { stdio: 'inherit' })
+
+        await new CLI().main(process, Operations.build, options.featureName, options.environment)
+    }
+
+    async #deploy(options) {
+        const command = `${Commander.#BABEL_COMMAND} && ${Commander.#PRETTIER_COMMAND} build/**/WebScreenSets/**/*.js`
+        execSync(command, { stdio: 'inherit' })
+
+        await new CLI().main(process, Operations.build, options.featureName, options.environment)
+        await new CLI().main(process, Operations.deploy, options.featureName, options.environment)
+    }
+
+    async #start() {
+        const command = `${Commander.#BABEL_COMMAND} && ${Commander.#PRETTIER_COMMAND} build/**/WebScreenSets/**/*.js`
+        execSync(command, { stdio: 'inherit' })
+
+        await new CLI().main(process, Operations.build)
+        execSync(Commander.#START_SERVER_COMMAND, { stdio: 'inherit' })
+    }
+
+    #createCommandWithSharedOptions(name) {
+        return new program.Command(name)
+            .option('-f, --featureName [featureName]', 'Single feature to be executed')
+            .option('-e, --environment [environment]', 'Configuration environment to be used during execution')
+    }
+
+    startProgram(process, name, description, version) {
+        const cmdInit = this.#createCommandWithSharedOptions(Operations.init).description('Reads the data from the cdc console of the sites configured').action(this.init)
+        const cmdReset = this.#createCommandWithSharedOptions(Operations.reset)
+            .description('Deletes the local folder and reads the data from the cdc console of the sites configured')
+            .action(this.#reset)
+        const cmdBuild = this.#createCommandWithSharedOptions(Operations.build)
+            .description('Processes the local data and prepares it to be deployed to the cdc console')
+            .action(this.#build)
+        const cmdDeploy = this.#createCommandWithSharedOptions(Operations.deploy)
+            .description('Writes the local data to the cdc console on the sites configured')
+            .action(this.#deploy)
+
+        program.name(name).description(description).version(version)
+        program.command(Operations.start).description('Launch local server for testing using the preview functionality').action(this.#start)
+        program.addCommand(cmdInit).addCommand(cmdReset).addCommand(cmdBuild).addCommand(cmdDeploy).parse(process.argv)
+    }
+}
