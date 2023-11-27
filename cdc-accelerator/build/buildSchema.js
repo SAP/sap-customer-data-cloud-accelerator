9755860add0eaeaaa8ff68072865e1a8754bddfe
Daniel Cabral <daniel.cabral@sap.com>
2023-11-27 18:59:41 +0000

    CDCINITIALIZER-159: Remove deprecated folders and scripts

--- cdc-accelerator/build/buildSchema.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-initializer contributors
- * License: Apache-2.0
- */
-const buildSchema = ({ srcDirectory, buildDirectory }) => {
-    // needs to be implemented
-}
-
-export { buildSchema }
