d4908582806474898b2441d5099a989451e41775
Bruno Santos <b.santos@sap.com>
2023-12-06 15:40:48 +0000

    CDCINITIALIZER-161: WIP

--- cdc-accelerator/feature/postInstall/projectSetup.js
+++ cdc-accelerator/feature/postInstall/projectSetup.js
@@ -6,6 +6,57 @@ export default class ProjectSetup {
     static SAP_ORG = '@sap_oss'
     static PACKAGE_JSON_FILE_NAME = 'package.json'
 
+    setup() {
+        const newProjectPackageJsonPath = ProjectSetup.PACKAGE_JSON_FILE_NAME
+        const newProjectPackageJson = JSON.parse(fs.readFileSync(newProjectPackageJsonPath, { encoding: 'utf8' }))
+        const dependencyName = this.#getAcceleratorDependencyName(newProjectPackageJson.devDependencies)
+        const acceleratorInstallationPath = path.join('node_modules', ...dependencyName.split(path.sep))
+
+        this.#uninstall(newProjectPackageJson, acceleratorInstallationPath)
+        this.#install(newProjectPackageJson, acceleratorInstallationPath)
+
+
+        // TODO setup upgrade
+        // TODO uninstall previous dependencies
+    }
+
+    #getAcceleratorDependencyName(devDependencies) {
+        const name = Object.keys(devDependencies).filter((dep) => dep.includes(ProjectSetup.SAP_ORG))
+        if (name[0] === undefined) {
+            throw new Error(`Cannot find ${ProjectSetup.SAP_ORG} dependency in package.json file`)
+        }
+        return name[0]
+    }
+
+    #uninstall(newProjectPackageJson, acceleratorInstallationPath) {
+
+    }
+
+    #install(newProjectPackageJson, acceleratorInstallationPath) {
+        this.#generatePackageJsonProperties(newProjectPackageJson, acceleratorInstallationPath)
+        this.#copyConfigurationFiles(acceleratorInstallationPath)
+        this.#generateConfigurationFile()
+        this.#generateIndexHtmlFile(acceleratorInstallationPath)
+
+        execSync('npm install', { stdio: 'inherit' })
+    }
+
+    #generatePackageJsonProperties(newProjectPackageJson, acceleratorInstallationPath) {
+        const acceleratorPackageJsonPath = path.join(acceleratorInstallationPath, ProjectSetup.PACKAGE_JSON_FILE_NAME)
+        const acceleratorPackageJson = JSON.parse(fs.readFileSync(acceleratorPackageJsonPath, { encoding: 'utf8' }))
+        this.#copyAcceleratorDependencies('devDependencies', newProjectPackageJson, acceleratorPackageJson)
+        this.#generateNpmScripts(newProjectPackageJson)
+        this.#generateJestConfiguration(newProjectPackageJson, acceleratorPackageJson)
+        this.#copyDependency('light-server', newProjectPackageJson, acceleratorPackageJson)
+        fs.writeFileSync(ProjectSetup.PACKAGE_JSON_FILE_NAME, JSON.stringify(newProjectPackageJson, null, 4))
+    }
+
+    #copyAcceleratorDependencies(dependenciesProperty, newProjectPackageJson, acceleratorPackageJson) {
+        Object.entries(acceleratorPackageJson[dependenciesProperty]).forEach((entry) => {
+            Object.assign(newProjectPackageJson[dependenciesProperty], { [entry[0]]: entry[1] })
+        })
+    }
+
     #generateNpmScripts(newProjectPackageJson) {
         return Object.assign(
             newProjectPackageJson['scripts'],
@@ -15,6 +66,7 @@ export default class ProjectSetup {
             this.#generateScript('deploy'),
             this.#generateScript('start'),
             this.#generateScript('setup'),
+            { test: "jest --collect-coverage --coverage --watch=all --coverageDirectory=coverage/unit ./src" }
         )
     }
 
@@ -22,20 +74,23 @@ export default class ProjectSetup {
         return { [name]: `npx cdc ${name}` }
     }
 
-    #copyAcceleratorDependencies(dependenciesProperty, newProjectPackageJson, acceleratorInstallationPath) {
-        const newProjectAcceleratorPackageJsonPath = path.join(acceleratorInstallationPath, ProjectSetup.PACKAGE_JSON_FILE_NAME)
-        const newProjectAcceleratorPackageJson = JSON.parse(fs.readFileSync(newProjectAcceleratorPackageJsonPath, { encoding: 'utf8' }))
-        Object.entries(newProjectAcceleratorPackageJson[dependenciesProperty]).forEach((entry) => {
-            Object.assign(newProjectPackageJson[dependenciesProperty], { [entry[0]]: entry[1] })
-        })
+    #generateJestConfiguration(newProjectPackageJson, acceleratorPackageJson) {
+        const key = 'jest'
+        newProjectPackageJson[key] = acceleratorPackageJson[key]
+        delete newProjectPackageJson[key].coveragePathIgnorePatterns
     }
 
-    #getAcceleratorDependencyName(devDependencies) {
-        const name = Object.keys(devDependencies).filter((dep) => dep.includes(ProjectSetup.SAP_ORG))
-        if (name[0] === undefined) {
-            throw new Error(`Cannot find ${ProjectSetup.SAP_ORG} dependency in package.json file`)
+    #copyDependency(dependency, newProjectPackageJson, acceleratorPackageJson) {
+        let dependencyContainer = 'devDependencies'
+        if(acceleratorPackageJson[dependencyContainer][dependency]) {
+            newProjectPackageJson[dependencyContainer][dependency] = acceleratorPackageJson[dependencyContainer][dependency]
         }
-        return name[0]
+        else {
+            dependencyContainer = 'dependencies'
+            if (acceleratorPackageJson[dependencyContainer][dependency]) {
+                newProjectPackageJson[dependencyContainer][dependency] = acceleratorPackageJson[dependencyContainer][dependency]
+            }
+        }
     }
 
     #copyConfigurationFiles(acceleratorInstallationPath) {
@@ -56,31 +111,34 @@ export default class ProjectSetup {
             source: [
                 {
                     apiKey: '',
-                },
+                }
             ],
             deploy: [
                 {
                     apiKey: '',
-                },
-            ],
+                }
+            ]
         }
         fs.writeFileSync(path.join('.', 'cdc-accelerator.json'), JSON.stringify(config, null, 4))
     }
 
-    #generateIndexHtmlFile() {}
+    #generateIndexHtmlFile(acceleratorInstallationPath) {
+        const templatePath = path.join(acceleratorInstallationPath, 'cdc-accelerator', 'templates', 'preview', 'index.html')
+        let content = fs.readFileSync(templatePath, { encoding: 'utf8' })
+        content = this.#replaceLinks(content, acceleratorInstallationPath)
+        fs.writeFileSync(path.join('src', 'index.html'), content)
+    }
 
-    setup() {
-        const newProjectPackageJsonPath = ProjectSetup.PACKAGE_JSON_FILE_NAME
-        const newProjectPackageJson = JSON.parse(fs.readFileSync(newProjectPackageJsonPath, { encoding: 'utf8' }))
-        const dependencyName = this.#getAcceleratorDependencyName(newProjectPackageJson.devDependencies)
-        const acceleratorInstallationPath = path.join('node_modules', ...dependencyName.split(path.sep))
-
-        this.#copyAcceleratorDependencies('devDependencies', newProjectPackageJson, acceleratorInstallationPath)
-        this.#generateNpmScripts(newProjectPackageJson)
-        fs.writeFileSync(ProjectSetup.PACKAGE_JSON_FILE_NAME, JSON.stringify(newProjectPackageJson, null, 4))
-        this.#copyConfigurationFiles(acceleratorInstallationPath)
-        this.#generateConfigurationFile()
-        this.#generateIndexHtmlFile()
-        execSync('npm install', { stdio: 'inherit' })
+    #replaceLinks(content, acceleratorInstallationPath) {
+        const oldLink = path.join('..', 'cdc-accelerator', 'preview')
+        const newLink = path.join('..', acceleratorInstallationPath, 'cdc-accelerator', 'preview')
+        let lines = content.split('\n')
+        lines = lines.map((line) => {
+            if (!line.includes(oldLink)) {
+                return line
+            }
+            line.replace(oldLink, newLink)
+        })
+        return lines
     }
 }
