c57abfbc98b041c4f8964be685bba7464aa27f11
Lopes <g.lopes@sap.com>
2024-01-04 14:03:13 +0000

    Adding Testing to terminal.js

--- cdc-accelerator/core/__tests__/terminal.test.js
+++ cdc-accelerator/core/__tests__/terminal.test.js
@@ -24,7 +24,22 @@ describe('Terminal test suite', () => {
             expectedOptions,
         )
     })
-
+    test('execute babel using the if statement is true', () => {
+        const spy = jest.spyOn(child_process, 'spawnSync').mockReturnValueOnce({ status: SUCCESS })
+        const expectedOptions = { shell: true, stdio: 'ignore' }
+        const srcFilePath = 'src' + path.sep
+        expect(Terminal.executeBabel(srcFilePath).status).toBe(SUCCESS)
+        expect(spy.mock.calls.length).toBe(1)
+        expect(child_process.spawnSync).toBeCalledWith('npx', ['babel', '--delete-dir-on-start', `\"src\"`, '-d', `\"build\"`], expectedOptions)
+    })
+    test('execute babel using the if statement is false', () => {
+        const spy = jest.spyOn(child_process, 'spawnSync').mockReturnValueOnce({ status: SUCCESS })
+        const expectedOptions = { shell: true, stdio: 'ignore' }
+        const srcFilePath = 'src'
+        expect(Terminal.executeBabel(srcFilePath).status).toBe(SUCCESS)
+        expect(spy.mock.calls.length).toBe(1)
+        expect(child_process.spawnSync).toBeCalledWith('npx', ['babel', '--delete-dir-on-start', `\"src\"`, '-d', `\"src\"`], expectedOptions)
+    })
     test('execute prettier', () => {
         const spy = jest.spyOn(child_process, 'spawnSync').mockReturnValueOnce({ status: SUCCESS })
         const expectedOptions = { shell: false, stdio: 'ignore' }
--- cdc-accelerator/core/terminal.js
+++ cdc-accelerator/core/terminal.js
@@ -4,19 +4,21 @@ import path from 'path'
 
 export default class Terminal {
     static executeBabel(directory) {
-        const exitCode = spawnSync('npx', ['babel', '--delete-dir-on-start', directory, '-d', directory.replace(SRC_DIRECTORY, BUILD_DIRECTORY)], {
-            shell: false,
+        let buildDir = directory.replace(SRC_DIRECTORY, BUILD_DIRECTORY)
+        if (directory.endsWith(path.sep)) {
+            directory = directory.substring(0, directory.length - 1)
+            buildDir = buildDir.substring(0, buildDir.length - 1)
+        }
+        return Terminal.executeCommand(`npx babel --delete-dir-on-start "${directory}" -d "${buildDir}"`, {
+            shell: true, // windows only executes commands on shell
             stdio: 'ignore',
         })
-        if (exitCode.status !== 0) {
-            throw new Error('Error executing babel')
-        }
-        return exitCode
     }
 
     static executePrettier(directory) {
+        // cannot call executeCommand because prettier command contains shell expansion characters
         const exitCode = spawnSync('npx', ['prettier', '--semi', 'true', '--trailing-comma', 'none', '--write', path.join(directory, '**', '*.js')], {
-            shell: false,
+            shell: true, // windows only executes commands on shell
             stdio: 'ignore',
         })
         if (exitCode.status !== 0) {
@@ -34,12 +36,20 @@ export default class Terminal {
         return exitCode
     }
 
-    static executeCommand(command, options = { shell: false, stdio: 'ignore' }) {
+    static executeCommand(command, options = { shell: true, stdio: 'inherit' }) {
+        // windows only executes commands on shell
+        if (Terminal.containsForbiddenCharacters(command)) {
+            throw new Error(`Error executing '${command}'. It contains shell expansion characters`)
+        }
         const tokens = command.split(' ')
         const exitCode = spawnSync(tokens[0], tokens.slice(1), options)
         if (exitCode.status !== 0) {
             throw new Error(`Error executing ${tokens.length > 1 ? tokens[1] : tokens[0]}`)
         }
         return exitCode
+    }
+
+    static containsForbiddenCharacters(command) {
+        return command.match(/[*?:;,&|+{}<>$`]/) ? true : false
     }
 }
