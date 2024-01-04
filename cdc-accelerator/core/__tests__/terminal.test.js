32b9be14fe948d06ec7c5c02dbb2478ee61b3aad
Lopes <g.lopes@sap.com>
2024-01-04 16:14:33 +0000

    CDCINITIALIZER-163:Adding babel testing

--- cdc-accelerator/core/__tests__/terminal.test.js
+++ cdc-accelerator/core/__tests__/terminal.test.js
@@ -24,18 +24,21 @@ describe('Terminal test suite', () => {
             expectedOptions,
         )
     })
-
-    test('execute babel when if is true', () => {
+    test('execute babel using the if statement is true', () => {
         const spy = jest.spyOn(child_process, 'spawnSync').mockReturnValueOnce({ status: SUCCESS })
         const expectedOptions = { shell: true, stdio: 'ignore' }
-
-        expect(Terminal.executeBabel(SRC_DIRECTORY).status).toBe(SUCCESS)
+        const srcFilePath = 'src' + path.sep
+        expect(Terminal.executeBabel(srcFilePath).status).toBe(SUCCESS)
         expect(spy.mock.calls.length).toBe(1)
-        expect(child_process.spawnSync).toBeCalledWith(
-            'npx',
-            ['babel', '--delete-dir-on-start', `"${SRC_DIRECTORY}"`, '-d', `"${SRC_DIRECTORY.replace(SRC_DIRECTORY, BUILD_DIRECTORY)}"`],
-            expectedOptions,
-        )
+        expect(child_process.spawnSync).toBeCalledWith('npx', ['babel', '--delete-dir-on-start', `\"src\"`, '-d', `\"build\"`], expectedOptions)
+    })
+    test('execute babel using the if statement is false', () => {
+        const spy = jest.spyOn(child_process, 'spawnSync').mockReturnValueOnce({ status: SUCCESS })
+        const expectedOptions = { shell: true, stdio: 'ignore' }
+        const srcFilePath = 'src'
+        expect(Terminal.executeBabel(srcFilePath).status).toBe(SUCCESS)
+        expect(spy.mock.calls.length).toBe(1)
+        expect(child_process.spawnSync).toBeCalledWith('npx', ['babel', '--delete-dir-on-start', `\"src\"`, '-d', `\"build\"`], expectedOptions)
     })
 
     test('execute prettier', () => {
