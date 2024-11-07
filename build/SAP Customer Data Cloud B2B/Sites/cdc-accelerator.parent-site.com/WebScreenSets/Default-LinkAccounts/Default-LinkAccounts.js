3fef23600c290e73371abb20a16c38b49aa69b93
Adolfo-Jorge-Sousa <adolfo.jorge.sousa@sap.com>
2024-11-07 17:23:36 +0000

    Criacao do novo site demo e modificacoes

--- build/SAP Customer Data Cloud B2B/Sites/cdc-accelerator.parent-site.com/WebScreenSets/Default-LinkAccounts/Default-LinkAccounts.js
+++ build/SAP Customer Data Cloud B2B/Sites/cdc-accelerator.parent-site.com/WebScreenSets/Default-LinkAccounts/Default-LinkAccounts.js
@@ -1,28 +1,31 @@
-{
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports['default'] = void 0;
+var _default = (exports['default'] = {
     // Called when an error occurs.
-    onError: function(event) {},
+    onError: function onError(event) {},
 
     // Called before validation of the form.
-    onBeforeValidation: function(event) {},
+    onBeforeValidation: function onBeforeValidation(event) {},
 
     // Called before a form is submitted. This event gives you an opportunity to perform certain actions before the form is submitted, or cancel the submission by returning false.
-    onBeforeSubmit: function(event) {},
+    onBeforeSubmit: function onBeforeSubmit(event) {},
 
     // Called when a form is submitted, can return a value or a promise. This event gives you an opportunity to modify the form data when it is submitted.
-    onSubmit: function(event) {},
+    onSubmit: function onSubmit(event) {},
 
     // Called after a form is submitted.
-    onAfterSubmit: function(event) {},
+    onAfterSubmit: function onAfterSubmit(event) {},
 
     // Called before a new screen is rendered. This event gives you an opportunity to cancel the navigation by returning false.
-    onBeforeScreenLoad: function(event) {},
+    onBeforeScreenLoad: function onBeforeScreenLoad(event) {},
 
     // Called after a new screen is rendered.
-    onAfterScreenLoad: function(event) {},
+    onAfterScreenLoad: function onAfterScreenLoad(event) {},
 
     // Called when a field is changed in a managed form.
-    onFieldChanged: function(event) {},
+    onFieldChanged: function onFieldChanged(event) {},
 
     // Called when a user clicks the "X" (close) button or the screen is hidden following the end of the flow.
-    onHide: function(event) {}
-}
+    onHide: function onHide(event) {}
+});
--- build/SAP Customer Data Cloud B2B/Sites/cdc-accelerator.parent-site.com/WebSdk/WebSdk.js
+++ build/SAP Customer Data Cloud B2B/Sites/cdc-accelerator.parent-site.com/WebSdk/WebSdk.js
@@ -1,24 +1,32 @@
-{
-  // A comma-delimited list of provider names to enable.
-  enabledProviders: '*',
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports['default'] = void 0;
+var _default = (exports['default'] = {
+    // A comma-delimited list of provider names to enable.
+    enabledProviders: '*',
 
-  // Define the language of Gigya's user interface and error message.
-  lang: 'en',
+    // Define the language of Gigya's user interface and error message.
+    lang: 'en',
 
-  // Bind globally to events.
-  customEventMap: {
-    eventMap: [{
-      events: '*',
-      args: [function (e) {
-        return e;
-      }],
-      method: function method(e) {
-        if (e.fullEventName === 'login') {
+    // Bind globally to events.
+    customEventMap: {
+        eventMap: [
+            {
+                events: '*',
+                args: [
+                    function (e) {
+                        return e;
+                    }
+                ],
 
-          // Handle login event here.
-        } else if (e.fullEventName === 'logout') {
-          // Handle logout event here.
-        }}
-    }]
-  }
-}
+                method: function method(e) {
+                    if (e.fullEventName === 'login') {
+                        // Handle login event here.
+                    } else if (e.fullEventName === 'logout') {
+                        // Handle logout event here.
+                    }
+                }
+            }
+        ]
+    }
+});
